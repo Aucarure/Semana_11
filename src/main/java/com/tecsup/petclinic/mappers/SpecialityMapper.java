5baf82a10dc453bdb57083ac7087cff0384b15a1
Aucarure <victor.aucarure@tecsup.edu.pe>
2025-11-03 17:05:42 -0500

    SpeacialityMapper

--- src/main/java/com/tecsup/petclinic/mappers/SpecialityMapper.java
+++ src/main/java/com/tecsup/petclinic/mappers/SpecialityMapper.java
@@ -1,4 +1,31 @@
 package com.tecsup.petclinic.mappers;
 
+import com.tecsup.petclinic.dtos.SpecialtyDTO;
+import com.tecsup.petclinic.entities.Specialty;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpecialityMapper {
+
+    public Specialty mapToEntity(SpecialtyDTO dto) {
+        if (dto == null) return null;
+        return new Specialty(
+                dto.getId(),
+                dto.getName(),
+                dto.getOffice(),
+                dto.getHOpen(),
+                dto.getHClose()
+        );
+    }
+
+    public SpecialtyDTO mapToDto(Specialty entity) {
+        if (entity == null) return null;
+        return new SpecialtyDTO(
+                entity.getId(),
+                entity.getName(),
+                entity.getOffice(),
+                entity.getHOpen(),
+                entity.getHClose()
+        );
+    }
 }
